6e41c6523f2919fc74abef0e722a2084954fc9df
Jonas Leite <139612792+JonasLeiteProgramador@users.noreply.github.com>
2023-12-15 15:28:26 -0300

    Delete Rede Neural - M3 PROJETO FINAL directory

--- Rede Neural - M3 PROJETO FINAL/Código das análises/5-Média de Avaliações por Projeto e Localização do Laboratório de Inovação.sql
+++
@@ -1,6 +0,0 @@
-SELECT descricao, Localizacao_Evento, AVG(Avaliacao_Colaboracao) as Avaliacao_Media
-FROM tb_Colaboracao
-JOIN tb_Projeto_e_Desafio ON tb_Colaboracao.ID_Projeto = tb_Projeto_e_Desafio.ID
-JOIN tb_Laboratorio_Inovacao ON tb_Projeto_e_Desafio.ID = tb_Laboratorio_Inovacao.ID
-GROUP BY descricao, Localizacao_Evento
-ORDER BY Avaliacao_Media DESC;
--- Rede Neural - M3 PROJETO FINAL/Código das análises/6-Habilidades Mentais mais Comuns com  Maiores Níveis de Proficiência.sql
+++
@@ -1,5 +0,0 @@
-SELECT Descricao AS HabilidadeMental, Nivel_Proficiencia
-FROM tb_Conexao_Habilidades
-JOIN tb_habilidade_mental ON tb_Conexao_Habilidades.ID_HabilidadeMental = tb_habilidade_mental.ID
-WHERE Nivel_Proficiencia >= 10
-ORDER BY Nivel_Proficiencia DESC;
